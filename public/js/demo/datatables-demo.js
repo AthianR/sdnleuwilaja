ad3f12720b958e9ebd4ad86ac36b1f77061c496d
AthianR <iyanthubet@gmail.com>
2023-08-07 22:48:38 +0700

    perbaikan tabel

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -32,6 +32,42 @@ function confirmDelete(type) {
 }
 
 $(document).ready(function () {
+    new DataTable("#dataSoal", {
+        initComplete: function () {
+            var table = this;
+
+            this.api()
+                .columns()
+                .every(function (index) {
+                    if (index === 1) {
+                        // Tentukan nomor indeks kolom yang ingin Anda terapkan filter seleksi di sini
+                        var column = this;
+                        var select = document.createElement("select");
+                        select.add(new Option(""));
+                        column.header().replaceChildren(select);
+
+                        select.addEventListener("change", function () {
+                            var val = DataTable.util.escapeRegex(select.value);
+
+                            column
+                                .search(val ? "^" + val + "$" : "", true, false)
+                                .draw();
+                        });
+
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.add(new Option(d));
+                            });
+                    }
+                });
+        },
+    });
+});
+
+$(document).ready(function () {
     new DataTable("#dataTableSiswa", {
         initComplete: function () {
             var table = this;
